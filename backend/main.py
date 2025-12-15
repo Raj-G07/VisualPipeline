0bfa9e84f91520b3b3042484c32ce9e22570eade
Raj Gupta <150777419+Raj-G07@users.noreply.github.com>
2025-12-15 15:07:52 +0530

    Config middleware

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from schemas.pipeline import PipelineRequest, PipelineResponse
 from services.pipeline_service import analyze_pipeline
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/pipelines/parse", response_model=PipelineResponse)
 def parse_pipeline(pipeline: PipelineRequest):
