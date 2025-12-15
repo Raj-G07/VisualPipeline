ea0e53721bd181b27287c907acbfba4b2b0e20d9
Raj Gupta <150777419+Raj-G07@users.noreply.github.com>
2025-12-15 15:17:13 +0530

    Check FE & BE integration

--- frontend/src/submit.js
+++ frontend/src/submit.js
@@ -1,10 +1,30 @@
 // submit.js
+import { fakeNodes, fakeEdges } from "./mocks/pipelineDagIsFalse";
 
 export const SubmitButton = () => {
 
+async function submitFakePipeline() {
+try {
+    const response = await fetch("http://127.0.0.1:8000/pipelines/parse", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        nodes: fakeNodes,
+        edges: fakeEdges
+      })
+    });
+  
+    const data = await response.json();
+    alert(JSON.stringify(data, null, 2));
+} catch (error) {
+    alert("Failed to submit pipeline. Check backend server.");
+    console.error(error);
+}
+}
+
     return (
         <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-            <button type="submit">Submit</button>
+            <button type="submit" onClick={submitFakePipeline}>Submit Pipeline</button>
         </div>
     );
 }
